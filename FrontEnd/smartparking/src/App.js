1de1f02f7c3e965de70011438890fb5661b0f364
Angelia TK <dreamcaster000@gmail.com>
2023-01-17 11:51:37 +0800

    routing

--- FrontEnd/smartparking/src/App.js
+++ FrontEnd/smartparking/src/App.js
@@ -1,5 +1,20 @@
+import PageLanding from "./Pages/PageLanding";
+import { Route, Routes } from "react-router-dom";
+import PageLogin from "./Pages/PageLogin";
+import PageRegister from "./Pages/PageRegister";
+import PageDashboard from "./Pages/PageDashboard";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<PageLanding />} />
+        <Route path="/login" element={<PageLogin />} />
+        <Route path="/register" element={<PageRegister />} />
+        <Route path="/dashboard" element={<PageDashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
