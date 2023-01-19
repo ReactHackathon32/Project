dce216ba1577ba61e9b740a7072a07b41dccecdd
Jared Ng <jarednjk@gmail.com>
2023-01-19 20:01:49 +0800

    New Parking Item commit
    styling for new parking item

--- FrontEnd/smartparking/src/Components/Dashboard/NewParkingItem.js
+++ FrontEnd/smartparking/src/Components/Dashboard/NewParkingItem.js
@@ -3,15 +3,25 @@ import { Button } from 'react-bootstrap'
 export const NewParkingItem = ({ id, name, dynamicPrice, avail }) => {
     return (
         <>
-            <div>New Parking Details</div>
-            <div>map</div>
-            <div>
-                <div className="text-center pb-4">Dynamic Pricing</div>
-                <div className="d-flex justify-content-center" >
-                    <Button style={{ width: "100px" }}>Ends At Time</Button>
-                    <Button style={{ width: "100px" }}>Cost Price</Button>
+            <h3 className='text-center mt-5 mb-4'>NEW PARKING DETAILS</h3>
+            <div className='map-image'></div>
+            <h4 className="text-center mt-5 mb-3">Dynamic Pricing</h4>
+            <div className='d-flex justify-content-center'>
+
+                <div className="d-flex justify-content-center rounded align-items-center" style={{backgroundColor: 'white', width: "300px", height: "150px"}}>
+                    <div className=''>
+                        
+                    
+                    </div>
+                    <div>
+
+                    </div>
+                    
                 </div>
             </div>
+            {/* <Button className='me-2' variant='dark'>Ends At<br />(TIME)</Button>
+                    <Button className='ms-2' variant='outline-dark'>Cost<br />(PRICE)</Button> */}
+                
             <h1>Carpark Name: {name}</h1>
             <h3>Current Price: {dynamicPrice}</h3>
             <p>Available Slots: {avail}</p>
